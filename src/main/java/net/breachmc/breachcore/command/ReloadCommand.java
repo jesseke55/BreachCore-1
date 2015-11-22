1612fbc9bcd17e47d3a22f77747e9d6d2ed562bf
Experminator <ronald4bunk@gmail.com>
2015-11-22 20:13:18 +0100

    Added reload command.

---
+++ src/main/java/net/breachmc/breachcore/command/ReloadCommand.java
@@ -0,0 +1,24 @@
+package net.breachmc.breachcore.command;
+
+import net.breachmc.breachcore.Main;
+import net.breachmc.breachcore.message.MessageManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Copyright (c) 2015, Experminator.
+ */
+public class ReloadCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("breach.reload")) {
+            Main.reload((args.length == 1 && args[0].equalsIgnoreCase("plugin")));
+            MessageManager.getMessage("reload-complete").send(sender);
+            return true;
+        }
+
+        return false;
+    }
+}
