053cea65d108a6342e5dfacede604b1518975553
Experminator <ronald4bunk@gmail.com>
2015-11-22 16:51:36 +0100

    Added Javadoc.

--- src/main/java/net/breachmc/breachcore/configuration/ConfigurationManager.java
+++ src/main/java/net/breachmc/breachcore/configuration/ConfigurationManager.java
@@ -17,7 +17,19 @@ public class ConfigurationManager {
     private File configurationFile;
     private FileConfiguration configuration;
 
-    protected ConfigurationManager(String fileName, FileType type) {
+    /**
+     * Access without any arguments is not allowed in this case.
+     */
+    private ConfigurationManager() {
+    }
+
+    /**
+     * Creates file with given name and extension, and makes the instance for the methods from here.
+     *
+     * @param fileName The name of the file.
+     * @param type     The type of the file.
+     */
+    public ConfigurationManager(String fileName, FileType type) {
         configurationFile = new File(Main.getPlugin().getDataFolder(), fileName + "." + type.getExtension());
 
         if (!(configurationFile.exists())) {
@@ -34,26 +46,55 @@ public class ConfigurationManager {
         configuration = YamlConfiguration.loadConfiguration(configurationFile);
     }
 
+    /**
+     * @param path The path to the object.
+     * @param <T> The object which is returned.
+     * @return T
+     */
     @SuppressWarnings("unchecked")
     public <T> T get(String path) {
         return (T) configuration.get(path);
     }
 
+    /**
+     * @param path The path in the configuration.
+     * @param value The value for the given path.
+     */
     public void set(String path, Object value) {
         configuration.set(path, value);
     }
 
+    /**
+     * @param path The path to remove from the configuration.
+     */
+    public void remove(String path) {
+        this.set(path, null);
+    }
+
+    /**
+     * @param path The path to check if it exists in the configuration.
+     * @return True if it exists in the configuration else false.
+     */
     public boolean contains(String path) {
         return configuration.contains(path);
     }
 
+    /**
+     * @see ConfigurationSection
+     * @param path The path to create the configuration section for.
+     * @return The ConfigurationSection with the given path.
+     */
     public ConfigurationSection createConfigurationSection(String path) {
         return configuration.createSection(path);
     }
 
-    public void save() {
+    /**
+     * @return True if successfully saved else false.
+     */
+    public boolean save() {
         try {
             configuration.save(configurationFile);
+            return true;
         } catch (IOException e) {
             throw new BreachException("Couldn't save '{file}': {reason}"
                     .replace("{file}", configurationFile.getName() + ".")
@@ -62,6 +103,10 @@ public class ConfigurationManager {
         }
     }
 
+    /**
+     * @see ConfigurationManager
+     * FileType enum for the ConfigurationManager constructor.
+     */
     public enum FileType {
 
         TEXT("txt"),
